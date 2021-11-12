ab4bc3501259ac496a7f7486785ea0dec5c4079c
Lorenzo Costa <lorenzocostasan94@gmail.com>
2021-11-12 11:32:55 -0300

    version-1.0

---
+++ src/components/Lista.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import './lista.css'
+
+export default class Lista extends Component {
+    state = {
+        data: []
+    }
+
+    async componentDidMount() {
+        const api = 'https://www.mocky.io/v2/5d531c4f2e0000620081ddce'
+
+        const response = await fetch(api)
+        console.log(" res: ", response)
+
+        const body = await response.json();
+        console.log("body:", body);
+
+        this.setState({ data: body })
+    }
+
+
+    render() {
+
+        const users = this.state.data
+
+        return (
+            <div className="containerList">{users.map((user) =>
+                <p className="usersList">
+                    <p className="containerImg"><div className="idUser"><strong > ID:</strong>{user.id}</div><img className="img" src={user.img} alt="" />
+                        <div className="userName"><strong> {user.username} </strong></div>
+                    </p>
+
+                    <h2 className="h2"> NOME: {user.name}</h2>
+
+                    <div className="divButton">
+                        <button className="button">Pagar</button>
+                    </div>
+                </p>)}
+            </div>
+        )
+
+    }
+
+
+
+}
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import ReactDOM from "react-dom"
+// import HeaderHero from "./components/firstHeader"
+import Lista from "./components/Lista"
+// import App from "./components/Lista"
+
+
+
+
+
+const rootComponent = document.getElementById("root")
+
+ReactDOM.render(
+
+   <div>
+
+      <Lista /> 
+
+   </div>
+
+   , rootComponent)
